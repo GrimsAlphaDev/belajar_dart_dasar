6212c69a495cda0032fd14115a68fbde6f4bd42e
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-05-27 23:49:01 +0700

    comment

---
+++ bin/comment.dart
@@ -0,0 +1,14 @@
+/// this is main function
+/// will be executed by dart
+
+void main() {
+  // this is text variable
+  var text = 'Apakah Ril Kawand ?';
+
+  /**
+   * this is multi line comment
+   * you can add more here
+   */
+
+  print(text);
+}
