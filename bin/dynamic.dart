e4bc8fe4c1dc67e3dd77ec62015fd47fea13eec8
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-05-28 00:20:12 +0700

    tipe data dynamic

---
+++ bin/dynamic.dart
@@ -0,0 +1,12 @@
+import 'package:belajar_dart_dasar/belajar_dart_dasar.dart';
+
+void main() {
+  dynamic variable = 100;
+  print(variable);
+
+  variable = true;
+  print(variable);
+
+  variable = 'text';
+  print(variable);
+}
