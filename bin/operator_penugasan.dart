980473588709848ce6dd0ac8bf4a0dfabdecb4cd
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-05-29 18:06:09 +0700

    Operator Penugasan & Icrement, Decrement

--- bin/operator_penugasan.dart
+++ bin/operator_penugasan.dart
@@ -5,4 +5,15 @@ void main() {
   // a -= 10;
 
   print(a);
+
+  // Increment & Decrement
+  var i = 0;
+  // i++; // i = i + 1
+
+  int j = i++; // j = i, i++
+  int k = ++i; // k = (i++)
+
+  print(i);
+  print(j);
+  print(k);
 }
