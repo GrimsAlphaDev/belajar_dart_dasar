abaae1ed0d398d4f0a32008a3855cb98f34fab06
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-05-30 22:04:47 +0700

    Function

---
+++ bin/function.dart
@@ -0,0 +1,16 @@
+// Function adalah blok kode program yang akan berjalan saat kita panggil
+// Function dapat memiliki parameter dan return value (Optional)
+
+// Buat Function (Void = tanpa return value)
+void sayHello(){
+  print("Hello World");
+}
+
+void main(List<String> args) {
+
+  // Panggil Function
+  sayHello();
+  sayHello();
+  sayHello();
+
+}
