3b0b683ee0bdb4b3af31295ba1d95432b43f209d
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-05-30 22:34:11 +0700

    Parameter Optional Dalam Function

---
+++ bin/function_optional_parameter.dart
@@ -0,0 +1,14 @@
+// Secara default, parameterwajib dikirim ketika kita memanggil function
+// namun jika kita ingin membuat parameter yang optional, artinya tidak wajib dikirim, kita bisa wrap dalam kurung ([])
+// Serta Parameter optional harus nullable
+// Parameter optional tidak bisa dikirim di parameter pertama
+
+void sayHello(String firstName, [String? mididleName ,String? lastName] ){
+  print("Hello $firstName $lastName");
+}
+
+void main(List<String> args) {
+  sayHello("Andi");
+  sayHello("Sri", "Santoso");
+  sayHello("Sri", "Santoso", "Sri");
+}
