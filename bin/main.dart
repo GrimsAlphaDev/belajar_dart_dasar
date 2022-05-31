1e40dfc29716a433c296d4413a62ebd47932e48c
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-05-31 20:48:02 +0700

    Menggunakan Parameter Dalam Main Function

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  print('Arguments : $args');
+}
+
+// Memasukan parameter di terminal
+
+// dart bin/main.dart args1 args2
+// Arguments : [args1, args2] //output
+
+// Bila argument mengandung spasi gunakan petik 2
+
+// dart bin/main.dart "args 1" "args 2"
+// Arguments : ["args 1", "args 2"] //output
